f29c4d151bb780cba7aaf3fc598bcf81ce3cade7
Matheus da Silva <ms25022003@gmail.com>
2021-12-03 11:58:43 -0300

    Enviando card do github

---
+++ src/components/ContribuitorsCards/index.jsx
@@ -0,0 +1,20 @@
+import { Card, Button } from "react-bootstrap";
+import batata from "../../assets/svg/perfil.svg"
+import gitHub from "../../assets/svg/git-hub.svg"
+import linkedin from "../../assets/svg/linkedin.svg"
+import "./styles.scss"
+
+export default function ContribuitorsCards({ img, name, sobrenome, linkdin, github }) {
+    return (
+        <Card className = "contribuitors-card">
+            <Card.Img variant="top" src={batata} />
+            <Card.Body>
+                <Card.Title> {name} </Card.Title>
+                <Card.Title> {sobrenome} </Card.Title>
+                <Button variant="primary" className="contribuitors-card-button" > <img src={ linkedin } alt="Logo-linkedin" /> Linkedin </Button>
+                <br/>
+                <Button variant="primary" className="contribuitors-card-button" > <img src={ gitHub } alt="Logo-git-hub" /> GitHub </Button>
+            </Card.Body>
+        </Card>
+    )
+}
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,15 +1,17 @@
 import { BrowserRouter, Routes } from 'react-router-dom';
 import Header from '../components/Header';
 import ProductCard from "../components/ProductCard"
+import ContribuitorsCard from "../components/ContribuitorsCards"
 
-export default function RouterList(){
-    return (
-        <BrowserRouter>
-          <Header/>
-          <ProductCard id = {1} title="titulo" description="descricao" price= {100.00}  />
-          <Routes>
+export default function RouterList() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <ProductCard id={1} title="titulo" description="descricao" price={100.00} />
+      <ContribuitorsCard name="Matheus" sobrenome="Silva" />
+      <Routes>
 
-          </Routes>
-        </BrowserRouter>
-    )
+      </Routes>
+    </BrowserRouter>
+  )
 }
