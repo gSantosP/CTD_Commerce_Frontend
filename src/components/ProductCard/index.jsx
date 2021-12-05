39165cc61884909f7afd46948f2f805116a8e134
aloisiot <aloisiot.c98@gmail.com>
2021-12-05 14:48:38 -0300

    ajustando tamanho da imagem do card de produto

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -1,16 +1,16 @@
+import "./styles.scss"
 import { Card, Button } from "react-bootstrap";
 import {useNavigate} from "react-router";
 
-// import batata from "../../assets/img/batata-doce-rosada.jpg"
-import "./styles.scss"
-//corrgir pra img
 export default function ProductCard({ id, title, price, image }) {
 
     const navigate = useNavigate();
 
     return (
         <Card className = "product-card">
-            <Card.Img variant="top" src={image} />
+            <div className="img-container">
+                <Card.Img variant="top" src={image} />
+            </div>
             <Card.Body>
                 <Card.Title> {title} </Card.Title>
                 <Card.Text>{`R$ ${price}`}</Card.Text>
